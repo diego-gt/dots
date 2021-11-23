d6ea2a9070f7025d7c28249c468bd2aaed62e329
Diego Garza <diego.gt.rh@gmail.com>
2021-11-23 01:40:40 -0600

    [bash] Add txt extension to files, remove bash.sh
    bash.sh is now two files, bashrc.txt and bash_aliases.txt

--- shells/bash.sh
+++
@@ -1,8 +0,0 @@
-alias ll='ls --color=auto --almost-all --human-readable -l'
-_BOLD="\[$(tput bold)\]"
-_BOLDITALIC="\[$(tput bold)$(tput sitm)\]"
-_RESET="\[$(tput sgr0)\]"
-_PURPLEFG="\[$(tput setaf 57)\]"
-
-export PS1="${_BOLDITALIC}${_PURPLEFG}\w $ ${_RESET}"
-
