31f2d01dc61ee3f250a5e1656e4ade399e107614
Diego Garza <diego.gt.rh@gmail.com>
2022-07-10 04:42:44 -0500

    add tree-sitter default parsers

--- nvim/init_linux.lua
+++ nvim/init_linux.lua
@@ -214,7 +214,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'typescript', 'rust', 'go', 'python' },
+  ensure_installed = { 'c', 'cpp', 'cmake', 'markdown', 'markdown_inline', 'html', 'json', 'make', 'lua', 'javascript', 'tsx', 'typescript', 'rust', 'toml', 'go', 'python' },
 
   highlight = { enable = true },
   indent = { enable = true },
